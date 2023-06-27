4e0d0bbd2fb4066f6a6534559eb97d791e9c68fd
Saqib <iamsaqib24@gmail.com>
2023-06-27 11:20:42 +0530

    seprated-stylesheet

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TextInput, View, Button, Alert, Pressable} from 'react-native';
+import styles from './App.styles';
 
 export default function App() {
-  console.log("Hello from console log!");
+  // console.log("Hello from console log!");
   return (
     <View style={styles.container}>
+      <Pressable style={styles.btn} onPress={() => Alert.alert("from pressable")}>
+        <Text>Press me</Text>
+      </Pressable>
       <Text>I'm Saqib, started learning React Native!</Text>
+      <TextInput
+        placeholder="Write here"
+        style={styles.input}
+      />
+      <Button
+        title="Click me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+        // color="#007AFF"
+        // disabled
+        style={styles.btn}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App.styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      borderWidth: 1,
+      backgroundColor: "lightgray",
+      color: "black",
+      padding: 10,
+      marginTop: 10,
+    },
+    btn: {
+      color: "#f194ff",
+      borderWidth: 1,
+      backgroundColor: "lightgray",
+      padding: 10,
+      marginTop: 10,
+      cursor: "pointer",
+    },
+  });
+
+  export default styles;
