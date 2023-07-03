a6cfbf78ee0d39d063fd68e05deb5092230b8589
Saqib <iamsaqib24@gmail.com>
2023-07-04 00:25:22 +0530

    Button-component-created

--- App.js
+++ App.js
@@ -4,11 +4,16 @@ import { Text, View, SafeAreaView} from 'react-native';
 import styles from './App.styles';
 import ImageOption from './src/components/ImageOption';
 import question from './assets/data/oneQuestionWithOption';
+import Button from './src/components/Button/Button';
 
 export default function App() {
   // console.log("Hello from console log!");
 
   const [selected, setSelected] = React.useState(null);
+
+  const onButtonPress = () => {
+    console.warn('pressed.')
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,6 +26,8 @@ export default function App() {
 
 
       </View>
+
+      <Button text='Check' onPress={onButtonPress} />
 
       <StatusBar style="auto" />
 
---
+++ src/components/Button/Button.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Pressable, Text } from "react-native";
+import styles from "./style";
+import PropTypes from 'prop-types';
+
+const Button = ({text, onPress}) => {
+    return (
+        <Pressable onPress={onPress} style={styles.container}>
+            <Text style={styles.text}>{text}</Text>
+        </Pressable>
+    )
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    onPress: PropTypes.func,
+}
+
+Button.defaultProps = {
+    onPress: () => {},
+}
+
+export default Button
